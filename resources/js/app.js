14bc4caa182c4edb7751a41116cdf39850245570
zhouweitong3 <xml@live.com>
2019-04-06 11:23:22 +0800

    Signed-off-by: zhouweitong3 <xml@live.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.antd = require('antd')
+window.$ = require('jquery')
+window.react = require('react')
+window.reactdom = require('react-dom')
 
 /**
  * The following block of code may be used to automatically register your
@@ -21,12 +25,3 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
-});
