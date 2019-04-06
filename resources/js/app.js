701ce16569a14a438712593874edfd08e4b9390c
zhouweitong3 <xml@live.com>
2019-04-06 19:21:11 +0800

    Signed-off-by: zhouweitong3 <xml@live.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,8 @@ require('./bootstrap');
 window.Vue = require('vue');
 window.antd = require('ant-design-vue');
 
-Vue.component('board', require('./components/board.vue'));
+Vue.component('board', require('./board.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
