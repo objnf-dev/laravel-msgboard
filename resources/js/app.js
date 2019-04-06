c53f7b3c57721618c552a9c20f783a06cf9f5a3f
zhouweitong3 <xml@live.com>
2019-04-06 12:02:01 +0800

    Signed-off-by: zhouweitong3 <xml@live.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,20 +8,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-window.antd = require('antd')
-window.$ = require('jquery')
-window.react = require('react')
-window.reactdom = require('react-dom')
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+window.antd = require('antd');
+import React from "react";
+import ReactDOM from "react-dom";
 
 
